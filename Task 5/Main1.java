e95806ef04d02109bbbeb26b311980e0c09834a5
nazmulhasanbabu2003 <157895574+nazmulhasanbabu2003@users.noreply.github.com>
2026-06-30 10:42:45 +0600

    Implement interfaces for multi-function copier

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,46 @@
+interface Printer {
+    void printDocument();
+}
 
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+class BasicPrinter implements Printer {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+public class task1 {
+    public static void main(String[] args) {
+        MultiFunctionCopier mfc = new MultiFunctionCopier();
+        BasicPrinter bp = new BasicPrinter();
+
+        mfc.printDocument();
+        mfc.scanDocument();
+        mfc.faxDocument();
+
+        bp.printDocument();
+    }
+}
