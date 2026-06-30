b8ae5866b3576d529c15dc950b44071bde1495f5
nazmulhasanbabu2003 <157895574+nazmulhasanbabu2003@users.noreply.github.com>
2026-06-30 10:37:45 +0600

    Implement interfaces for HTTP, Database, and Docker services

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,49 @@
+interface HttpService {
+    void handleHttpRequest();
+}
 
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+class WebController implements HttpService {
+
+    @Override
+    public void handleHttpRequest() {
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+class DatabaseService implements DatabaseManager {
+
+    @Override
+    public void executeSqlStatement() {
+        System.out.println("Executing SQL statement.");
+    }
+}
+
+class DockerService implements ContainerOps {
+
+    @Override
+    public void restartDockerContainer() {
+        System.out.println("Restarting Docker container.");
+    }
+}
+
+public class task6 {
+    public static void main(String[] args) {
+
+        WebController controller = new WebController();
+        controller.handleHttpRequest();
+
+        DatabaseService db = new DatabaseService();
+        db.executeSqlStatement();
+
+        DockerService docker = new DockerService();
+        docker.restartDockerContainer();
+    }
+}
