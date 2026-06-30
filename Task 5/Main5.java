e045e1e0a7a5ffad329469bd156b7c5932ea9299
nazmulhasanbabu2003 <157895574+nazmulhasanbabu2003@users.noreply.github.com>
2026-06-30 10:38:48 +0600

    Implement university member classes and interfaces

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,45 @@
+class UniversityMember {
 
+    public void attendLecture() {
+        System.out.println("Attending a lecture.");
+    }
+}
+
+interface Researcher {
+    void conductLabResearch();
+}
+
+class UndergraduateStudent extends UniversityMember {
+}
+
+class GraduateResearchAssistant extends UniversityMember implements Researcher {
+
+    @Override
+    public void conductLabResearch() {
+        System.out.println("Publishing papers and running experiments.");
+    }
+}
+
+class Professor extends UniversityMember implements Researcher {
+
+    @Override
+    public void conductLabResearch() {
+        System.out.println("Publishing papers and running experiments.");
+    }
+}
+
+public class task5 {
+    public static void main(String[] args) {
+
+        UndergraduateStudent student = new UndergraduateStudent();
+        student.attendLecture();
+
+        GraduateResearchAssistant gra = new GraduateResearchAssistant();
+        gra.attendLecture();
+        gra.conductLabResearch();
+
+        Professor professor = new Professor();
+        professor.attendLecture();
+        professor.conductLabResearch();
+    }
+}
